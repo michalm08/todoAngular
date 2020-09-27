1f4c2d63cca98b476fc7eaa8ce797b7c33c519e8
michalm08 <michalmski@gmail.com>
2020-09-27 14:25:36 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('hello');
   });
 
+
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'hello';
+  todoArray = [];
+
+  addTodo(value): void{
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+    value = ' ';
+  }
+
+  clear(value): void{
+    value.value = ' ';
+  }
+
+
 }
+
+
+
+
+
+
+//
+// todoArray = []; addTodo(value){
+//   this.todoArray.push(value);
+//   console.log(this.todos);
+// }
+
